2918da52a9131d5c955aa65d43ab151d7ceb9997
michael <v.b.n@mail.ru>
2016-10-25 16:49:17 +0300

    Stub: barman pour 200 grams of whisky

--- test/whenBarmanWantsToPourWhiskyStubTests.js
+++ test/whenBarmanWantsToPourWhiskyStubTests.js
@@ -10,7 +10,20 @@ suite('Stub: when client ask 200 grams of whisky', function () {
         client.sober();
     })
     suite('barman has enough', function () {
-
+        let cupboardStub = {
+            hasDrink: function(){
+                return true;
+            },
+            getDrink: function(){
+                return 200;
+            }
+        }
+        let barman = new Barman(cupboardStub);
+        test('Barman pour 200 gra,s of whisky',function(){
+            let clientAskValume = 200;
+            let volumeInGlass =  barman.pour(drinkName,clientAskValume,client);
+            assert.equal(clientAskValume,volumeInGlass);
+        })
     });
 
     suite('no whisky in bar', function () {
